c93c327c2c7039ab1d822b3cef66513a3db2b191
Ludvig Hagmar <ludvig.hagmar@prisjakt.nu>
2024-11-10 11:45:51 +0100

    Primevue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>hej</div>
+  <h1>Escaper</h1>
+  <form @submit.prevent="onSubmit">
+    <div class="flex flex-col gap-1">
+      <Textarea rows="20" />
+    </div>
+    <Button type="submit" severity="secondary" label="Submit" />
+  </form>
 </template>
 
 <script setup lang="ts">
-console.log("hej");
+import Textarea from "primevue/textarea";
+import Button from "primevue/button";
+
+const onSubmit = () => {
+  console.log("submit");
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+textarea {
+  width: 100%;
+}
+</style>
